8cc87e7ec70d7f741587f76372edb190363a4dfe
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-06-19 13:49:42 -0400

    can pass methods also as props, so you can call a method that can change the state in another component that doesn’t have direct access to the state

--- src/App.js
+++ src/App.js
@@ -28,9 +28,16 @@ class App extends Component {
       <div className="App">
         <h1> click some buttons / state management </h1>
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name= {this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name= {this.state.persons[1].name} age={this.state.persons[1].age}> Hobbies: Racing </Person>
-        <Person name= {this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person 
+          name= {this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name= {this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler}> Hobbies: Racing </Person>
+        <Person 
+          name= {this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
   }
