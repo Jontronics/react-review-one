be35071b872d4ae61369339bf9b7f91d0006166f
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-06-19 13:27:04 -0400

    added a button to change state through props

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person.js'
+
+class App extends Component {
+  
+  state = {
+    persons: [
+      {name: 'Jon', age: 28},
+      {name: 'Chris', age: 33},
+      {name: 'Nick', age: 5}
+    ]
+  }
+  
+  switchNameHandler = () => {
+    // console.log("this works")
+      this.setState({persons: [
+        {name: 'Jonathan', age: 28},
+        {name: 'Chris', age: 33},
+        {name: 'Nick', age: 57}
+      ]
+    })
+  }
+  
+  
+  render() {
+    return (
+      <div className="App">
+        <h1> click some buttons / state management </h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name= {this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name= {this.state.persons[1].name} age={this.state.persons[1].age}> Hobbies: Racing </Person>
+        <Person name= {this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
+}
+
+
+
+
+export default App;
